07c24a87c2b872cf9b5b3fbe1453a0683f38f82e
Kevin Malazarte <kevin.malazarte@gmail.com>
2019-03-31 17:54:36 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,7 +3,7 @@ require "pry"
 def nyc_pigeon_organizer(data)
   pigeon_list = {}
   data.each do |attribute, detail_hash|
-    detail_hash.each do 
+    detail_hash.each do |key, array|
     binding.pry
   end
 end
